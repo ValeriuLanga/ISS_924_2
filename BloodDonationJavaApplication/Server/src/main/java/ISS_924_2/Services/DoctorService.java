799c0c13c22ac8703ced17f6e121996601863b98
Valeriu Langa <vlanga@bitdefender.com>
2018-04-22 17:14:16 +0300

    * placed the java project in a file * set up the java sdk version

---
+++ BloodDonationJavaApplication/Server/src/main/java/ISS_924_2/Services/DoctorService.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class DoctorService {
+
+    /**
+     * Default constructor
+     */
+    public DoctorService() {
+    }
+
+    /**
+     * 
+     */
+    public Doctor CurrentUser;
+
+    /**
+     * 
+     */
+    private Repository DoctorRepository;
+
+
+    /**
+     * 
+     */
+    public void CheckStatus() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void RequestBlood() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CancelBloodRequest() {
+        // TODO implement here
+    }
+
+}
---
+++ BloodDonationJavaApplication/Server/src/main/java/ISS_924_2/SpringServer/SpringServer.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class SpringServer {
+
+    /**
+     * Default constructor
+     */
+    public SpringServer() {
+    }
+
+    /**
+     * 
+     */
+    private DonorService donorService;
+
+    /**
+     * 
+     */
+    private PersonnelService personnelService;
+
+    /**
+     * 
+     */
+    private DoctorService doctorService;
+
+    /**
+     * 
+     */
+    private AdminService adminService;
+
+
+
+
+
+    /**
+     * 
+     */
+    public void HandleRequests() {
+        // TODO implement here
+    }
+
+    /**
+     * @param User
+     */
+    private void validateUserCredentials(void User) {
+        // TODO implement here
+    }
+
+}
