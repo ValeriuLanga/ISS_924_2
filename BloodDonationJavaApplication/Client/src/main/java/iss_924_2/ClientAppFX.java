d2ee0a80a84f21fc59df942013ec5c8edba687d5
Andrei Petrut Manaila <andreimanaila27@gmail.com>
2018-05-22 12:29:56 +0300

    Added Doctor Window

---
+++ BloodDonationJavaApplication/Client/src/main/java/iss_924_2/ClientAppFX.java
@@ -0,0 +1,25 @@
+package iss_924_2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientAppFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/fx/LoginWindow.fxml"));
+        Scene scene = new Scene(root);
+        mainStage.setTitle("Blood Donation");
+        mainStage.setScene(scene);
+        mainStage.setMinHeight(700);
+        mainStage.setMinWidth(1000);
+        mainStage.setFullScreen(true);
+        mainStage.show();
+    }
+}
