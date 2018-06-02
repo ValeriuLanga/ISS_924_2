ab9a3ac7d0347bb60ef6c5086610070c19f17051
Andrei Petrut Manaila <andreimanaila27@gmail.com>
2018-06-02 17:07:31 +0300

    Added authentication window

--- BloodDonationJavaApplication/Client/src/main/java/iss_924_2/ClientAppFX.java
+++ BloodDonationJavaApplication/Client/src/main/java/iss_924_2/ClientAppFX.java
@@ -7,19 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ClientAppFX extends Application {
+    Parent root;
+    Stage stage;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage mainStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/fx/LoginWindow.fxml"));
-        Scene scene = new Scene(root);
-        mainStage.setTitle("Blood Donation");
-        mainStage.setScene(scene);
-        mainStage.setMinHeight(700);
-        mainStage.setMinWidth(1000);
-        mainStage.setFullScreen(true);
-        mainStage.show();
+    public void start(Stage mainStage) {
+        try {
+            root = FXMLLoader.load(getClass().getResource("../../resources/fx/AuthenticationWindow.fxml"));
+            stage = mainStage;
+            stage.setTitle("Blood Donation");
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
