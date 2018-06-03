a6bc556e81ae54466b483e222ca705b88b5acc78
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2018-06-03 15:35:32 +0300

    * partially implemented DonorServiceServer

--- BloodDonationJavaApplication/Core/src/main/java/iss_924_2/service/DonorService.java
+++ BloodDonationJavaApplication/Core/src/main/java/iss_924_2/service/DonorService.java
@@ -12,33 +12,29 @@ public interface DonorService {
     /**
      *
      */
-    Set<Analysis> viewBloodAnalysisHistory();
+    Set<Analysis> viewBloodAnalysisHistory(int id);
 
     /**
      *
      */
-    void register();
+    //username, password, firstName, lastName, dOb, street, number, city, country, --actual address
+    void register(String username, String password, String firstName, String lastName, Date dateOfBirth, String street, String number, String city, String country, String actualStreet, String actualNumber, String actualCity, String actualCountry);
 
     /**
      *
      */
-    Date viewNextDonationDate();
+    Date viewNextDonationDate(int id);
 
     /**
      * @param donor
      *
      */
-    void changePersonalInformation(Donor donor);
-
-    /**
-     *
-     */
-    void changeDonationInfo();
+    void changePersonalInformation(int id, Donor donor);
 
     /**
      * @param name
      *
      */
-    void donateToSpecificPerson(String name);
+    void donateToSpecificPerson(int id, String name);
 
 }
--- BloodDonationJavaApplication/Server/src/main/java/iss_924_2/services/DonorServiceServer.java
+++ BloodDonationJavaApplication/Server/src/main/java/iss_924_2/services/DonorServiceServer.java
@@ -1,78 +1,76 @@
 package iss_924_2.services;
 
+import iss_924_2.domain.Address;
 import iss_924_2.domain.Analysis;
 import iss_924_2.domain.Donor;
+import iss_924_2.domain.User;
+import iss_924_2.repository.DonorRepository;
 import iss_924_2.repository.Repository;
 import iss_924_2.service.DonorService;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 
  */
 public class DonorServiceServer implements DonorService {
 
-    /**
-     * Default constructor
-     */
     public DonorServiceServer() {
     }
 
-    /**
-     * 
-     */
-    public Donor currentUser;
-
-    /**
-     * 
-     */
     private Repository donorRepository;
 
+    private Repository analysisHistory;
 
-    /**
-     * 
-     */
-    public Set<Analysis> viewBloodAnalysisHistory() {
+    public Set<Analysis> viewBloodAnalysisHistory(int id) {
         // TODO implement here
+
+        List<Analysis> analysisList = analysisHistory.findAll();
+//
+//        for(Analysis analysis: analysisList){
+//            if(id == analysis)
+//        }
+
         return null;
     }
 
-    /**
-     * 
-     */
     public Date viewNextDonationDate() {
         // TODO implement here
+
+
         return null;
     }
 
-    /**
-     * 
-     */
-    public void register() {
+    public void register(String username, String password, String firstName, String lastName, Date dateOfBirth, String street, String number, String city, String country, String actualStreet, String actualNumber, String actualCity, String actualCountry){
         // TODO implement here
+        User donor = new Donor();
+
+        donor.setUserName(username);
+        donor.setPassword(password);
+        donor.setFirstName(firstName);
+        donor.setLastName(lastName);
+        ((Donor) donor).setDateOfBirth(dateOfBirth);
+        Address address = new Address(street, number, city, country);
+        Address actualAddress = new Address(actualStreet, actualNumber, actualCity, actualCountry);
+        ((Donor) donor).setAddress(address);
+        ((Donor) donor).setActualAddress(actualAddress);
+
+        donorRepository.save(donor);
+
     }
 
-    /**
-     * 
-     */
-    public void changePersonalInformation(Donor donor) {
+    public void changePersonalInformation(int id, Donor donor) {
         // TODO implement here
+
+
+
     }
 
-    /**
-     *
-     */
-    public void changeDonationInfo() {
-        // TODO implement here
-    }
-
-
-    /**
-     * 
-     */
     public void donateToSpecificPerson(String name) {
         // TODO implement here
+
+
     }
 
 }
