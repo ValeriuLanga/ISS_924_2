486f9067ad2f1e5408e783fa2bc7b5562b407db0
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2018-06-08 00:33:55 +0300

    * implemented exceptions for date and phone number * updated validator

---
+++ BloodDonationJavaApplication/Core/src/main/java/iss_924_2/core/exceptions/InvalidPhoneNumberException.java
@@ -0,0 +1,6 @@
+package iss_924_2.core.exceptions;
+
+public class InvalidPhoneNumberException extends RuntimeException{
+
+    public InvalidPhoneNumberException(String message) { super(message); }
+}
--- BloodDonationJavaApplication/Core/src/main/java/iss_924_2/core/validator/Validator.java
+++ BloodDonationJavaApplication/Core/src/main/java/iss_924_2/core/validator/Validator.java
@@ -1,7 +1,9 @@
 package iss_924_2.core.validator;
 
 import iss_924_2.core.exceptions.InvalidAgeException;
+import iss_924_2.core.exceptions.InvalidDateException;
 import iss_924_2.core.exceptions.InvalidNameException;
+import iss_924_2.core.exceptions.InvalidPhoneNumberException;
 
 import java.util.regex.Pattern;
 
@@ -31,5 +33,44 @@ public final class Validator {
 
         if(age > 60)
             throw new InvalidAgeException("Age is over 60, no donations allowed!");
+    }
+
+    public static void validateDate(String date) {
+
+        String dd, mm, yyyy;
+        String[] parts = date.split("-");
+        dd = parts[0];
+        mm = parts[1];
+        yyyy = parts[2];
+
+        if(dd.length() != 2)
+            throw new InvalidDateException("The day format is invalid! Correct format is 'dd-mm-yyyy'");
+
+        if(mm.length() != 2)
+            throw new InvalidDateException("The month format is invalid! Correct format is 'dd-mm-yyyy'");
+
+        if(yyyy.length() != 4)
+            throw new InvalidDateException("The year format is invalid! Correct format is 'dd-mm-yyyy'");
+
+        int day, month;
+        day = Integer.parseInt(dd);
+        month = Integer.parseInt(mm);
+
+        if(day < 1 && day > 31)
+            throw new InvalidDateException("Day is not valid!");
+        if(month < 1 && month > 12)
+            throw new InvalidDateException("Month is not valid!");
+        if(month == 2)
+            if(day > 28)
+                throw new InvalidDateException("Day is not valid!")
+
+    }
+
+    public static void validatePhoneNumber(String phoneNumber) {
+
+        if(phoneNumber.length() != 10)
+            throw new InvalidPhoneNumberException("Phone number must have 10 digits!");
+        if(phoneNumber.charAt(0) == '0')
+            throw new InvalidPhoneNumberException("Phone number must start with '0'!");
     }
 }
